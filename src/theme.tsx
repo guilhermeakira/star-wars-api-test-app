a957344916075022ad4e300c7dab70b445a36d60
Guilherme Akira Matsumoto <guilherme.matsumoto@quintoandar.com.br>
2020-08-27 22:51:18 -0300

    custom theme

---
+++ src/theme.tsx
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2C97D1',
+    },
+  },
+});
+
+export default theme;
