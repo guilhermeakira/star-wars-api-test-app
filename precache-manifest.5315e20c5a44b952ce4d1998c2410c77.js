995c75b08d3a325c6d48713a4c97bd96f172240a
Guilherme Akira Matsumoto <guilherme.matsumoto@quintoandar.com.br>
2020-08-30 19:21:56 -0300

    Updates

---
+++ precache-manifest.5315e20c5a44b952ce4d1998c2410c77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96498ce724f2d684b32e2cd720975615",
+    "url": "/star-wars-api-test-app/index.html"
+  },
+  {
+    "revision": "12e00e95a0a414806b7f",
+    "url": "/star-wars-api-test-app/static/css/main.7cedcd52.chunk.css"
+  },
+  {
+    "revision": "7abcb1e7f34cd2b838e2",
+    "url": "/star-wars-api-test-app/static/js/2.12386eef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/star-wars-api-test-app/static/js/2.12386eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e00e95a0a414806b7f",
+    "url": "/star-wars-api-test-app/static/js/main.d9ffe8c9.chunk.js"
+  },
+  {
+    "revision": "f0d347ca0917b7993549",
+    "url": "/star-wars-api-test-app/static/js/runtime-main.5d4d4aa1.js"
+  },
+  {
+    "revision": "3e06b3a8400a31f968fb37d4a368c9fe",
+    "url": "/star-wars-api-test-app/static/media/darth-vader.3e06b3a8.png"
+  },
+  {
+    "revision": "738fc4804a1b605797968b2f1446769f",
+    "url": "/star-wars-api-test-app/static/media/luke-skywalker.738fc480.png"
+  }
+]);
