cec192acbec0aa3416b714de37557406c7cc6936
Guilherme Akira Matsumoto <guilherme.matsumoto@quintoandar.com.br>
2020-08-30 18:27:16 -0300

    updating theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -3,27 +3,48 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#2C97D1',
+      main: '#2A2A2A',
+      contrastText: '#FBFE63',
     },
+    secondary: {
+      main: '#FFFFFF',
+      contrastText: '#2B2B2B',
+    },
+    text: {
+      primary: '#2C97D1',
+    },
+    common: {
+      black: '#000000',
+      white: '#FFFFFF'
+    },
+  },
+  shape: {
+    borderRadius: 10,
   },
   typography: {
     fontFamily: [
       'Montserrat',
     ].join(','),
     h1: {
-      "fontSize": "4.5rem",
-      "fontWeight": "normal",
+      'fontSize': '4.5rem',
+      'fontWeight': 'normal',
       '@media (max-width:900px)': {
         fontSize: '1.5rem',
       },
     },
+    subtitle1: {
+      'fontSize': '2.25em',
+      'lineHeight': '1.25em',
+    },
     subtitle2: {
-      "fontWeight": "normal",
-      "letterSpacing": "0.35em",
-      "lineHeight": "1.25em",
+      'fontWeight': 'normal',
+      'letterSpacing': '0.35em',
+      'lineHeight': '1.25em',
     },
     button: {
-      "fontWeight": 'bold',
+      'fontSize': '1.125rem',
+      'lineHeight': '1.25em',
+      'fontWeight': 'bold',
     },
   }
 });
