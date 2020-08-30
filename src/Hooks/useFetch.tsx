d6f3fac34e69917a6448d3225bf8703d85f33a42
Guilherme Akira Matsumoto <guilherme.matsumoto@quintoandar.com.br>
2020-08-30 18:26:53 -0300

    creating hooks folder

---
+++ src/Hooks/useFetch.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { FETCH_STATUS } from '../constants';
+
+const useFetch = (shouldFetch: boolean = true, initialMaster = '') => {
+  const [status, setStatus] = useState(FETCH_STATUS.IDLE);
+  const [masterName, setMasterName] = useState(initialMaster);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setStatus(FETCH_STATUS.FETCHING);
+      const fetchLuke = fetch('https://swapi.dev/api/people/1');
+      const fetchVader = fetch('https://swapi.dev/api/people/4');
+      const response = await Promise.race([fetchLuke, fetchVader]);
+
+      const data = await response.json();
+
+      setMasterName(data.name);
+      setStatus(FETCH_STATUS.FETCHED);
+    };
+
+    if (shouldFetch) {
+      fetchData();
+    }
+  }, [shouldFetch]);
+
+  return { status, masterName };
+};
+
+export default useFetch;
